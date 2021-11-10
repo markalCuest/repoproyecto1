714347a4c43a76013367f0f5eb7e0240ab3ad3a9
Marcos <76123467+markalCuest@users.noreply.github.com>
2021-11-10 16:34:29 +0100

    Commit realizado el dia 10 Noviembre de 2021.

---
+++ src/AppBundle/Controller/Calculator2Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+/**
+ * Description of Calculator2Controller
+ *
+ * @author Daw
+ */
+
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Route;
+
+class Calculator2Controller extends Controller{
+    
+    public function indexAction($age){
+        $currentYear = date("Y");
+        $year = $currentYear  - $age;
+        return $this ->render("calculator/index.html.twig", array("year" =>$year));
+    }
+
+}
